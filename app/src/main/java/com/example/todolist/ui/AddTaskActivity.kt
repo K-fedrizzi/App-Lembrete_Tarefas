12cfcec93d0582e21dd5d3ba5c20f3cc8e8e6d9b
Karina Fedrizzi <karina.fedrizzi91@gmail.com>
2021-08-25 12:06:10 -0300

    Mostrando as lista de tarefas no memu e as opções de editar e deletar sem implementações

--- app/src/main/java/com/example/todolist/ui/AddTaskActivity.kt
+++ app/src/main/java/com/example/todolist/ui/AddTaskActivity.kt
@@ -1,5 +1,6 @@
 package com.example.todolist.ui
 
+import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -63,6 +64,8 @@ class AddTaskActivity : AppCompatActivity() {
                 hour = binding.tilHour.text
             )
            TaskDataSource.insertTask(task)
+
+           setResult(Activity.RESULT_OK)
            finish()
        }
 
