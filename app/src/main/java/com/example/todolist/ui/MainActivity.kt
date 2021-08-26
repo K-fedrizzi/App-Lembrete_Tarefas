09720ccf7b4f268fa568053ccaf9cfd1e6a62482
Karina Fedrizzi <karina.fedrizzi91@gmail.com>
2021-08-25 21:00:07 -0300

    projeto concluido

--- app/src/main/java/com/example/todolist/ui/MainActivity.kt
+++ app/src/main/java/com/example/todolist/ui/MainActivity.kt
@@ -1,51 +1,68 @@
 package com.example.todolist.ui
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import com.example.todolist.databinding.ActivityMainBinding
 import com.example.todolist.datasource.TaskDataSource
 
 class MainActivity : AppCompatActivity() {
 
-   private lateinit var binding: ActivityMainBinding
-   private val adapter by lazy {TasklListAdapter() }
+    private lateinit var binding: ActivityMainBinding
+    private val adapter by lazy { TasklListAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
 
         binding.rvTasks.adapter = adapter
+        updateList()
 
-        insertListerners()
-
+        insertListeners()
     }
 
-    private fun insertListerners() {
-        binding.fab.setOnClickListener{
-            startActivityForResult(Intent(this, AddTaskActivity::class.java), CREATE_NEW_TASK)
+    private fun insertListeners() {
+        binding.fab.setOnClickListener {
+            startActivityForResult(Intent(this,AddTaskActivity::class.java),  CREATE_NEW_TASK)
         }
 
-        adapter.listenerEdit ={
-            Log.e("TAG", "listenerEdit: $it")
+        adapter.listenerEdit = {
+            val intent = Intent(this,AddTaskActivity::class.java)
+            intent.putExtra(AddTaskActivity.TASK_ID, it.id)
+            startActivityForResult(intent, CREATE_NEW_TASK)
         }
 
-        adapter.listenerEdit={
-            Log.e("TAG", "listenerDelete: $it")
+        adapter.listenerDelete = {
+            TaskDataSource.deleteTask(it)
+            updateList()
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode  == CREATE_NEW_TASK) {
-            binding.rvTasks.adapter = adapter
-            adapter.submitList(TaskDataSource.getList())
+        if(requestCode == CREATE_NEW_TASK && resultCode == Activity.RESULT_OK) updateList()
+
+    }
+
+    private fun updateList() {
+        val list = TaskDataSource.getList()
+        if(list.isEmpty()){
+            binding.includeEmpty.emptyState.visibility = View.VISIBLE
+        }else{
+            binding.includeEmpty.emptyState.visibility = View.GONE
+            binding.rvTasks.visibility = View.VISIBLE
         }
+
+        adapter.submitList(list)
     }
 
     companion object{
-        private const val  CREATE_NEW_TASK = 1000
+        private const val CREATE_NEW_TASK = 1000
     }
 }
